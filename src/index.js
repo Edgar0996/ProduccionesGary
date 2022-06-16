d0fd833759a2b30c92feced490a04348c92cd776
Edgar Vázquez López <71537168+Edgar0996@users.noreply.github.com>
2022-06-16 18:39:47 -0500

    Archivos css,html,js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATinXCUaAg2UK8gk0P71cvnhWf7i-OxSI",
+  authDomain: "produccionesgaribay.firebaseapp.com",
+  projectId: "produccionesgaribay",
+  storageBucket: "produccionesgaribay.appspot.com",
+  messagingSenderId: "58645271857",
+  appId: "1:58645271857:web:2f642811f17c41b325cc10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
